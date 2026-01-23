f7b5e62bb170747a032ade55b052c5b19341f9f2
Yousuf7900 <yousufislam7900@gmail.com>
2026-01-23 17:59:36 +0600

    Initial commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const Router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Hello</h1>
+    }
+])
+
+export default Router;
